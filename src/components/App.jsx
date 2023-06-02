30ea8e5b36d37f097e834a11a9d6ad3a7c0e32b5
MariySemenenko <mashyni92@gmail.com>
2023-06-02 21:53:33 +0300

    comit

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -7,17 +7,17 @@ import { Filter } from './Filter/Filter';
 
 
 export class App extends Component {
-  state = {
+  state = { //визначаю два поля масив і рядок
     contacts: [
       { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
       { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
       { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
       { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
     ],
-    filter: '',
+    filter: '', //пошук контактів за іменем
   };
 
-  filterChange = e => {
+  filterChange = e => {      //цей метод викликається при зміні значення фільтру і оновлюється коли користувач вводе значення
     this.setState({ filter: e.target.value });
   };
 
@@ -25,32 +25,24 @@ export class App extends Component {
     const { filter, contacts } = this.state;
     const normalizedFilter = filter.toLowerCase();
 
-    return contacts.filter(contact =>
+    return contacts.filter(contact => //повертаємо відфільтрований масив 
       contact.name.toLowerCase().includes(normalizedFilter)
     );
   };
 
   addContact = newContact => {
-   
+    //перевіряю чи існує контакт з однаковим ід
+    const existingContact = this.state.contacts.find(contact => contact.id === newContact.id);
 
-   
-    const nameToLowerCase = newContact.name.toLowerCase();
-
-    if (
-      this.state.contacts.find(
-        contact =>
-          contact.name.toLowerCase() === nameToLowerCase ||
-          contact.number === newContact.number
-      )
-    ) {
-      alert(`${newContact.name}  ${newContact.number} is already in contacts`);
-      return;
+    if (existingContact) { //якщо існує контакт
+        alert(`${newContact.name} ${newContact.number} is already in contacts`);
+        return;
     }
 
-    this.setState(prevState => ({
-      contacts: [newContact, ...prevState.contacts],
+    this.setState(prevState => ({ //якщо не існує то, оновлюємось та додаємось до попереднього масиву
+        contacts: [newContact, ...prevState.contacts],
     }));
-  };
+};
 
   deleteContacts = id => {
     this.setState(prevState => ({
@@ -80,3 +72,25 @@ export class App extends Component {
     );
   }
 }
+
+
+// or
+// addContact = newContact => {  //функція додавання контактів
+   
+//   const nameToLowerCase = newContact.name.toLowerCase();
+
+//   if (
+//     this.state.contacts.find(  //перевіряю чи існує контакт з таким іменем
+//       contact =>
+//         contact.name.toLowerCase() === nameToLowerCase ||
+//         contact.number === newContact.number
+//     )
+//   ) {
+//     alert(`${newContact.name}  ${newContact.number} is already in contacts`);
+//     return;
+//   }
+
+//   this.setState(prevState => ({
+//     contacts: [newContact, ...prevState.contacts],
+//   }));
+// };
